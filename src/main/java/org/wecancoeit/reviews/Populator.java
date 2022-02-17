a28918bca3c68bd2ca3c9bdac09c5cf4a3e3de11
Dallas Baldwin <Dbaldw10@kent.edu>
2022-02-17 15:11:41 -0500

    ajsdklfjsd

--- src/main/java/org/wecancoeit/reviews/Populator.java
+++ src/main/java/org/wecancoeit/reviews/Populator.java
@@ -17,14 +17,44 @@ public class Populator implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        Console console = new Console("Sega Genesis", "Sega", "08/14/1989", "Console");
-        consoleRepo.save(console);
+        Console console1 = new Console("Sega Genesis", "Sega", "08/14/1989", "Console");
+        consoleRepo.save(console1);
         Console console2 = new Console("Nintendo Gameboy", "Nintendo", "1989", "Handheld");
         consoleRepo.save(console2);
         Console console3 = new Console("Nintendo SNES", "Nintendo", "11/21/1990", "Console");
         consoleRepo.save(console3);
-        Console console4 = new Console("Xbox", "Microsoft", "11/15/2001", "Console");
+        Console console4 = new Console("Sega Gamegear", "Sega", "10/6/1990", "Handheld");
         consoleRepo.save(console4);
+        Console console5 = new Console("Playstation (1)", "Sony", "9/16/1995", "Console");
+        consoleRepo.save(console5);
+        Console console6 = new Console("Sega Dreamcast", "Sega", "11/27/1998", "Console");
+        consoleRepo.save(console6);
+        Console console7 = new Console("Playstation 2", "Sony", "3/4/2000", "Console");
+        consoleRepo.save(console7);
+        Console console8 = new Console("Xbox (First gen)", "Microsoft", "11/15/2001", "Console");
+        consoleRepo.save(console8);
+        Console console9 = new Console("Nintendo DS", "Nintendo", "11/21/2004", "Handheld");
+        consoleRepo.save(console9);
+        Console console10 = new Console("Xbox 360", "Microsoft", "11/21/2005", "Console");
+        consoleRepo.save(console10);
+        Console console11 = new Console("Playstation 3", "Sony", "11/11/2006", "Console");
+        consoleRepo.save(console11);
+        Console console12 = new Console("Nintendo Wii", "Nintendo", "11/19/2006", "Console");
+        consoleRepo.save(console12);
+        Console console13 = new Console("PS Vita", "Sony", "12/17/2011", "Handheld");
+        consoleRepo.save(console13);
+        Console console14 = new Console("Playstation 4", "Sony", "11/15/2013", "Console");
+        consoleRepo.save(console14);
+        Console console15 = new Console("Xbox One", "Microsoft", "11/22/2013", "Console");
+        consoleRepo.save(console15);
+        Console console16 = new Console("Nintendo Switch", "Nintendo", "3/3/2017", "Handheld");
+        consoleRepo.save(console16);
+        Console console17 = new Console("Xbox Series X", "Microsoft", "11/10/2020", "Console");
+        consoleRepo.save(console17);
+        Console console18 = new Console("Playstation 5", "Sony", "11/10/2020", "Console");
+        consoleRepo.save(console18);
+
+
 
         Manufacturer manufacturer = new Manufacturer("Microsoft", console4);
         manufacturerRepo.save(manufacturer);
