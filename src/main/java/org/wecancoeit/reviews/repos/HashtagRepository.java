79b49d6837a7fe30c7fd3e0e39dbfa499fe26245
Dallas Baldwin <Dbaldw10@kent.edu>
2022-02-23 14:23:39 -0500

    added /hashtags

--- src/main/java/org/wecancoeit/reviews/repos/HashtagRepository.java
+++ src/main/java/org/wecancoeit/reviews/repos/HashtagRepository.java
@@ -1,7 +1,11 @@
 package org.wecancoeit.reviews.repos;
 
 import org.springframework.data.repository.CrudRepository;
+import org.wecancoeit.reviews.entities.Console;
 import org.wecancoeit.reviews.entities.Hashtag;
+
+import java.util.Collection;
+import java.util.Optional;
 
 public interface HashtagRepository extends CrudRepository<Hashtag,Long> {
 }
